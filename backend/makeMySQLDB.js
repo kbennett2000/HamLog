a9471d310a5fbc7418a66c9ddfe7cbb21f95bc0a
Kris Bennett <kbennett2000@hotmail.com>
2024-01-14 20:09:26 -0700

    Create makeMySQLDB.js

---
+++ backend/makeMySQLDB.js
@@ -0,0 +1,20 @@
+// MySQL Create Database
+async function CreateDatabase() {
+  const dbName = "HamLogDB";
+  const mysql = require("mysql2/promise"); // MySQL library for asynchronous operations
+  // Create a MySQL connection
+  const connection = await mysql.createConnection({
+    host: "192.168.0.235",
+    user: "testUser",
+    password: "password1",
+  });
+  // Insert data into the ConditionReports table
+  await connection.execute(`CREATE DATABASE ${dbName}`);
+
+  console.log(`Database ${dbName} created.`);
+
+  // Close the MySQL connection
+  await connection.end();
+}
+
+CreateDatabase();

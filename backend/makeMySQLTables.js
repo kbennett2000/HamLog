f299586b96eb6f289267c1534f4fb9d51984cbcd
Kris Bennett <kbennett2000@hotmail.com>
2024-01-16 13:08:59 -0700

    Update makeMySQLTables.js

--- backend/makeMySQLTables.js
+++ backend/makeMySQLTables.js
@@ -1,29 +1,33 @@
 async function CreateTables() {
   const dbName = "HamLogDB";
-  const table1Name = "Contacts";
-  const table2Name = "POTA";
   const mysql = require("mysql2/promise"); // MySQL library for asynchronous operations
   // Create a MySQL connection
   const connection = await mysql.createConnection({
-    host: "192.168.0.235",
+    host: "192.168.1.85",
     user: "testUser",
     password: "password1",
     database: `${dbName}`,
   });
 
-  // TODO: set proper field names
-  // Insert data into the ConditionReports table
   await connection.execute(
-    `CREATE TABLE ${table1Name} (QSO_ID VARCHAR(255), QSO_Date VARCHAR(255), QSO_UTCTime VARCHAR(255), QSO_MTZTime VARCHAR(255), QSO_Callsign VARCHAR(255), QSO_Frequency VARCHAR(255), QSO_Notes VARCHAR(4096))`
+    "CREATE TABLE `HamLogDB`.`Contacts` (`QSO_ID` INT AUTO_INCREMENT, `QSO_Date` DATETIME NULL, `QSO_MTZTime` VARCHAR(12) NULL, `QSO_Callsign` VARCHAR(12) NULL, `QSO_Frequency` VARCHAR(15) NULL, `QSO_Notes` VARCHAR(4096) NULL, `QSO_Received` VARCHAR(10) NULL, `QSO_Sent` VARCHAR(10) NULL, PRIMARY KEY (`QSO_ID`));"
   );
-  console.log(`Database Table ${table1Name} created.`);
+  console.log("Contacts table created.");
 
-  // TODO: set proper field names
-  // Insert data into the ConditionReports table
   await connection.execute(
-    `CREATE TABLE ${table2Name} (POTA_OSO_ID VARCHAR(255), QSO_ID VARCHAR(255), POTAPark_ID VARCHAR(255), QSO_Type VARCHAR(255))`
+    "CREATE TABLE `HamLogDB`.`POTA_QSOs` (`POTA_QSO_ID` INT AUTO_INCREMENT, `QSO_ID` INT, `POTAPark_ID` VARCHAR(10), `QSO_Type` VARCHAR(2), PRIMARY KEY (`POTA_QSO_ID`), FOREIGN KEY (`QSO_ID`) REFERENCES `Contacts`(`QSO_ID`));"
   );
-  console.log(`Database Table ${table2Name} created.`);
+  console.log("POTA_QSOs table created.");
+
+  await connection.execute(
+    "CREATE TABLE `HamLogDB`.`Contests` (`CONTEST_ID` INT AUTO_INCREMENT, `CONTEST_NAME` VARCHAR(512), `CONTEST_DESCRIPTION` VARCHAR(1024), `CONTEST_BEGINS_ON_DATE` DATETIME NULL, `CONTEST_BEGINS_ON_TIME` DATETIME NULL, `CONTEST_ENDS_ON_DATE` DATETIME NULL, `CONTEST_ENDS_ON_TIME` DATETIME NULL, `CONTEST_EXCHANGE_DATA` VARCHAR(1024) NULL, PRIMARY KEY (`CONTEST_ID`));"
+  );
+  console.log("Contests table created.");
+
+  await connection.execute(
+    "CREATE TABLE `HamLogDB`.`Contest_QSOs` (`CONTEST_QSO_ID` INT AUTO_INCREMENT, `QSO_ID` INT, `CONTEST_ID` INT, `CONTEST_QSO_NUMBER` VARCHAR(10), `CONTEST_QSO_EXCHANGE_DATA` VARCHAR(128), PRIMARY KEY (`CONTEST_QSO_ID`), FOREIGN KEY (`QSO_ID`) REFERENCES `Contacts`(`QSO_ID`), FOREIGN KEY (`CONTEST_ID`) REFERENCES `Contests`(`CONTEST_ID`));"
+  );
+  console.log("Contest_QSOs table created.");
 
   // Close the MySQL connection
   await connection.end();

41bc70ac930074e10d968b73490a063b25004b2d
Kris Bennett <kbennett2000@hotmail.com>
2024-01-22 13:32:53 -0700

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ContactsControl from "./controls/ContactsControl";
+import "./App.css";
+
+const App = () => {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (tabNumber) => {
+    setActiveTab(tabNumber);
+  };
+
+  return (
+    <div className="container mx-auto mt-8">
+      <div className="flex bg-gray-100" style={{ borderRadius: '8px' }}>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 1 ? "border-blue-500 text-blue-500 bg-blue-50" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(1)}>Contacts</button>
+      </div>
+
+      <div className="tab-content mt-4">
+        {activeTab === 1 && <ContactsControl />}
+      </div>
+    </div>
+  );
+};
+
+export default App;

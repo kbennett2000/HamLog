203d2bc64066fe436d94ef192ed209816846ef61
kbennett2000 <kbennett2000@hotmail.com>
2024-01-22 16:59:58 -0700

    Added Redux for state management

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,3 @@
-
-// store.js
 import { createStore } from 'redux';
 
 // Define an initial state
@@ -21,32 +19,3 @@ const reducer = (state = initialState, action) => {
 const store = createStore(reducer);
 
 export default store;
-
-
-/*
-// store.js
-import { configureStore } from '@reduxjs/toolkit';
-
-// You can create a slice or just use a simple reducer as before
-const initialState = {
-    lastInsertID: ''
-};
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'SET_LAST_INSERT_ID':
-            return { ...state, lastInsertID: action.payload };
-        default:
-            return state;
-    }
-};
-
-const store = configureStore({
-    reducer: {
-        // If you have multiple reducers, you can add them here
-        lastInsertIDReducer: reducer,
-    },
-});
-
-export default store;
-*/
